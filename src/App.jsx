e99317903fd04a0b40f8985393ac635a75be8c31
Ayodeji <thimmzieayodeji@gmail.com>
2026-02-12 12:31:15 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+function App() {
+  <Routes>
+    <Route path="/" element={<Home />} />
+  </Routes>;
+}
+
+export default App;
