2d4ff21dcc5a710e5d07f254f1c61ba1e80ae6c6
Ayodeji <thimmzieayodeji@gmail.com>
2026-02-14 00:00:19 +0100

    signin changes

--- src/assets/components/signIn.jsx
+++ src/assets/components/signIn.jsx
@@ -27,7 +27,7 @@ const signIn = () => {
       <div className="px-6 md:px-23 lg:bg-[#f3f4f6] lg:pt-40 lg:px-32">
         <div className="flex flex-col items-center mt-10">
           <img className="lg:h-[4vh]" src={Logo} />
-          <h1 className="text-[#1f2937] text-[1.9rem] font-bold mt-1.3 lg:text-[1.4rem]">
+          <h1 className="text-[#1f2937] text-[1.8rem] font-bold mt-1.3 lg:text-[1.4rem]">
             Login your Account
           </h1>
         </div>
@@ -35,47 +35,49 @@ const signIn = () => {
           <div className="relative">
             <MdEmail
               size={20}
-              className="absolute left-3 top-17 lg:top-14.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
+              className="absolute left-3 top-15.5 lg:top-14.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
             />
 
-            <label className="text-[1.2rem] lg:text-[0.9rem]">
+            <label className="text-[1rem] lg:text-[0.9rem]">
               Email or phone number
             </label>
             <input
               type="text"
               placeholder="name@gmail.com"
-              className="w-full h-[9vh] lg:h-[7.5vh] text-[#3b4758] text-[1.3rem] lg:text-[0.9rem] pl-10 rounded-xl border border-[#3b3b3b] outline-0 mt-2 "
+              className="w-full h-[7.5vh] lg:h-[7.5vh] text-[#3b4758] text-[1rem] lg:text-[0.9rem] pl-10 rounded-xl border border-[#3b3b3b] outline-0 mt-2 "
             />
           </div>
           <div className="mt-4 relative">
             <IoMdLock
               size={20}
-              className="absolute left-3 top-16 lg:top-13.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
+              className="absolute left-3 top-14 lg:top-13.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
             />
             <FaEyeSlash
               size={19}
-              className="absolute right-4 top-17 lg:top-13.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
+              className="absolute right-4 top-15 lg:top-13.5 -translate-y-1/2 text-[#3b4758] pointer-events-none"
             />
-            <label className="text-[1.2rem] lg:text-[0.9rem]">Password</label>
+            <label className="text-[1rem] lg:text-[0.9rem]">Password</label>
             <input
               type="password"
               placeholder="******"
-              className="w-full h-[9vh] lg:h-[7.5vh] text-[#3b4758] text-[1.3rem] pl-10 rounded-xl border border-[#3b3b3b] outline-0 mt-2 "
+              className="w-full h-[7.5vh] lg:h-[7.5vh] text-[#3b4758] text-[1rem] pl-10 rounded-xl border border-[#3b3b3b] outline-0 mt-2 "
             />
           </div>
         </form>
         <div className="flex justify-end mt-2">
-          <a className="text-[#61aaea] text-[1rem] lg:text-[0.8rem]" href="#">
+          <a className="text-[#61aaea] text-[0.8rem] lg:text-[0.8rem]" href="#">
             Forgot Password?
           </a>
         </div>
         <div>
-          <button className="bg-[#ff7a18] text-[#ffffff] text-[1rem] lg:text-[0.9rem] w-full h-[9vh] rounded-xl hover:bg-[#ffffff] hover:text-[#ff7a18] hover:cursor-pointer transition duration-500 ease-in-out hover:border-2 hover:border-[#ff7a18] mt-4">
+          <button className="bg-[#ff7a18] text-[#ffffff] text-[1rem] lg:text-[0.9rem] w-full h-[7.5vh] rounded-xl hover:bg-[#ffffff] hover:text-[#ff7a18] hover:cursor-pointer transition duration-500 ease-in-out hover:border-2 hover:border-[#ff7a18] mt-4">
             Continue
           </button>
           <div className="flex items-center mt-2 gap-2">
             <span className="flex-1 h-[0.5px] bg-[#dbdbdb]"></span>
-            <p className=" px-1 lg:text-[0.7rem]">Or continue with</p>
+            <p className=" px-1 text-[0.8rem] lg:text-[0.7rem]">
+              Or continue with
+            </p>
             <span className="flex-1 h-[0.5px] bg-[#dbdbdb]"></span>
           </div>
         </div>
